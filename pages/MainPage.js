3b41d97f57d3acc5d4330ca7b34e6a19d2e38934
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-04 04:18:32 +0900

    feat : 상품 검색 기능 구현 완료

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -4,7 +4,22 @@ import { View, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 export default function MainPage() {
 
+    //데이터
+    const [items, setItems] = React.useState([
+        { id: '1', name: '흑하이덴 2*10*500', price: 3000 },
+        { id: '2', name: '치마비닐 1*60*200(나일론줄)', price: 3000 },
+        { id: '3', name: '흑색유공 2*150*300(15*25)', price: 3000 },
+    ]);
+
+
+    //검색어
     const [searchQuery, setSearchQuery] = React.useState('');
+
+    //검색로직
+    const searchItems = items.filter(item =>
+        item.name.includes(searchQuery.trim())
+    );
+
 
     // 메뉴를 위한거
     const [visible, setVisible] = React.useState(false);
@@ -55,28 +70,22 @@ export default function MainPage() {
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={{ flex: 1, padding: 16 }}>
 
+                    {/* 아이템 리스트 */}
                     <List.Section>
-                        <List.Item
-                            titleNumberOfLines={5}
-                            title="흑하이덴 2*10*500이고 테스트용으로 기이이이이이이이이일게 써봄"
-                            right={() => <Text style={{ alignSelf: 'center' }}>₩3,000</Text>}
-                        />
 
-                        <Divider />
+                        {searchItems.map(item => (
+                            <View key={item.id}>
+                                <List.Item
+                                    titleNumberOfLines={5}
+                                    title={item.name}
+                                    right={() => <Text style={{ alignSelf: 'center' }}>₩ {item.price.toLocaleString('ko-KR')}</Text>}
+                                />
 
-                        <List.Item
-                            title="치마비닐 1*60*200(나일론줄)"
-                            right={() => <Text style={{ alignSelf: 'center' }}>₩3,000</Text>}
-                        />
+                                <Divider />
+                            </View>
+                        ))}
 
-                        <Divider />
-
-                        <List.Item
-                            title="흑색유공 2*150*300(15*25)"
-                            right={() => <Text style={{ alignSelf: 'center' }}>₩3,000</Text>}
-                        />
                     </List.Section>
-
 
                 </View>
             </TouchableWithoutFeedback>
