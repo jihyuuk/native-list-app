5e46ecd7bf32d11c6089c67dbe1ad0a9b5a7a942
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-08 07:58:37 +0900

    build : 첫 버전 빌드

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import { PaperProvider } from 'react-native-paper';
 import MainPage from './pages/MainPage';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PaperProvider>
-        <MainPage />
-      </PaperProvider>
+      <SafeAreaProvider>
+        <StatusBar style="dark" backgroundColor="#fff" />
+        <PaperProvider>
+          <MainPage />
+        </PaperProvider>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
--- utils/csvExport.js
+++ utils/csvExport.js
@@ -1,5 +1,3 @@
-// utils/csvExport.js
-
 import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import { Alert } from 'react-native';
